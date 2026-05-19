15ff562922ebea48eda37ce8e0a39cba22a43f2d
Ankita Kumari <ak95349135@gmail.com>
2026-05-19 17:38:09 +0530

    fxed logo render

--- frontend/src/Sidebar.jsx
+++ frontend/src/Sidebar.jsx
@@ -2,7 +2,7 @@ import "./Sidebar.css";
 import { useContext, useEffect } from "react";
 import { MyContext } from "./MyContext.jsx";
 import {v1 as uuidv1} from "uuid";
-import logo from "../assets/blacklogo.png";
+
 
 function Sidebar() {
     const {allThreads, setAllThreads, currThreadId, setNewChat, setPrompt, setReply, setCurrThreadId, setPrevChats} = useContext(MyContext);
@@ -68,7 +68,7 @@ function Sidebar() {
     return (
         <section className="sidebar">
             <button onClick={createNewChat}>
-                <img src={logo} alt="logo" className="logo" />
+                <img src="/logo.png" alt="logo" className="logo" style={{ width: "40px", height: "40px" }} />
                 <span><i className="fa-solid fa-pen-to-square"></i></span>
             </button>
 
